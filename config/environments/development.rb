73c7119715661eb3e9c2a5d2f5f3f45aa5e71ff7
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-23 15:43:14 -0400

    Add devise setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,21 +2,20 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+	
   # Do not eager load code on boot.
   config.eager_load = false
-
+	
   # Show full error reports.
   config.consider_all_requests_local = true
-
+	
   # Enable server timing
   config.server_timing = true
-
+	
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
@@ -35,6 +34,9 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+	
+	# Default url options
+	config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
