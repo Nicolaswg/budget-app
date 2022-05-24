42646e2a6740dcd117da9bc4dc47f2561e56bf62
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-24 10:58:41 -0400

    Add splash screen setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'splash/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+	
   # Defines the root path route ("/")
   # root "articles#index"
+	unauthenticated :user do
+		root "splash#index", as: :unauthenticated_root
+	end
 end
