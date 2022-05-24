b7f7c3c958a351e4347f65a3de623029f3c590c4
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-24 14:11:49 -0400

    Update schema

--- db/migrate/20220523185025_create_groups.rb
+++ db/migrate/20220523185025_create_groups.rb
@@ -3,15 +3,12 @@ class CreateGroups < ActiveRecord::Migration[7.0]
     create_table :groups do |t|
       t.string :name
       t.string :icon
-			t.decimal :total_ammount, default: 0
       t.integer :author_id
-      t.integer :agreement_id
+			t.decimal :total_ammount, default: 0
 
       t.timestamps
     end
 		add_index :groups, :author_id
-		add_index :groups, :agreement_id
 		add_foreign_key :groups, :users, column: :author_id, on_delete: :cascade
-		add_foreign_key :groups, :agreements, column: :agreement_id, on_delete: :cascade
   end
 end
