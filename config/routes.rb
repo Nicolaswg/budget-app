09af94f3815caa73df57b8238182666f1a506ac1
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-25 13:01:24 -0400

    Update validation || Apply style to categories page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 	
   # Defines the root path route ("/")
   # root "articles#index"
+	
+	devise_scope :user do
+		get '/users/sign_out', to: 'devise/sessions#destroy'
+	end
+	
 	authenticated :user do
 		root "groups#index", as: :authenticated_root
 	end
